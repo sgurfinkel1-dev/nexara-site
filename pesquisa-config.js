b50c6147d817031629cc9e62429b8c7d9bfb589e
sgurfinkel1-dev <sgurfinkel1-dev@users.noreply.github.com>
2026-08-22 21:47:13 -0300

    Connect national survey to v5 backend

--- pesquisa-config.js
+++ pesquisa-config.js
@@ -1,2 +1,2 @@
 // Preenchido após a implantação do Web App na planilha da Beth.
-window.NEXARA_SURVEY_ENDPOINT = "";
+window.NEXARA_SURVEY_ENDPOINT = "https://script.google.com/macros/s/AKfycbz3FrCmb49NWHTsZSrKDyobp1gUfhltK8hZK610e0RHH5Bxb1NLP1aS9udqPGhaV0yA/exec";
